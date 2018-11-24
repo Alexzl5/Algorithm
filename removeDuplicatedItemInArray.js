d1abd24402fca2a6d004408e6d1c64199062865a
Alexzl5 <zl20130520@gmail.com>
2018-11-25 00:37:22 +0800

    using indexof & push to achieve the goal was added

--- removeDuplicatedItemInArray.js
+++ removeDuplicatedItemInArray.js
@@ -1,3 +1,6 @@
+/** 
+
+// using splice to remove the repeated item
 Array.prototype.DeleteRepeatedItem = function(){
     for(let i = this.length-1;i>=0;i--){
         for(let j = 0; j<this.length; j++){
@@ -8,6 +11,19 @@ Array.prototype.DeleteRepeatedItem = function(){
     }
     return this
 }
+*/
 
-// let arr = [1,2,2,5,3,6,7,1,6,2,9,20,2,1,2]
-// console.log(arr.DeleteRepeatedItem())
+// using indexof & push & create new empty array to remove repeated file
+
+Array.prototype.DeleteRepeatedItem = function() {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (arr.indexOf(this[i]) === -1) {
+      arr.push(this[i]);
+    }
+  }
+  return arr
+};
+
+let arr = [1,2,2,5,3,6,7,1,6,2,9,20,2,1,2]
+console.log(arr.DeleteRepeatedItem())
